1325621860acdf4fb3899bfb9a7b32d01694b54a
Aryan Chaurasiya <aryanchaurasiya0123@gmail.com>
2026-06-20 01:20:35 +0530

    my all things

--- Experiments/Experiment07/main.jsx
+++ Experiments/Experiment07/main.jsx
@@ -2,8 +2,7 @@
  import './StudentCard.css';
  function StudentCard(props) {
    return (
-         <div className="student-card">
-          
+         <div className="student-card"> 
       <h2>Student Details</h2>
       <p><strong>Name:</strong> {props.name}</p>
       <p><strong>Course:</strong> {props.course}</p>
