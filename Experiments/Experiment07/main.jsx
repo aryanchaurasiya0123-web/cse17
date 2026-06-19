8af7690723f269cae525ada24dd758ce9205d777
Aryan Chaurasiya <aryanchaurasiya0123@gmail.com>
2026-06-20 01:19:27 +0530

    my all things

--- Experiments/Experiment07/main.jsx
+++ Experiments/Experiment07/main.jsx
@@ -3,6 +3,7 @@
  function StudentCard(props) {
    return (
          <div className="student-card">
+          
       <h2>Student Details</h2>
       <p><strong>Name:</strong> {props.name}</p>
       <p><strong>Course:</strong> {props.course}</p>
