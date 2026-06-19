a9d7d45a3905bfc2ea14c25c9e3003dd8b43574f
Aryan Chaurasiya <aryanchaurasiya0123@gmail.com>
2026-06-20 01:25:03 +0530

    my all things

--- Experiments/Experiment07/main.jsx
+++ Experiments/Experiment07/main.jsx
@@ -4,8 +4,7 @@
    return (
          <div className="student-card"> 
       <h2>Student Details</h2>
-      <p><strong>Name:</strong> {props.name}</p>
-      
+      <p><strong>Name:</strong> {props.name}</p> 
       <p><strong>Course:</strong> {props.course}</p>
       <p><strong>Marks:</strong> {props.marks}</p>
     </div>   
